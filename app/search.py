62f3d9a45b91df18eb05728cece31803fb2ccca8
Sam Borremans <samborremans.y@gmail.com>
2024-12-05 10:14:53 -0500

    sorting and filtering in search completed

--- app/search.py
+++ app/search.py
@@ -9,10 +9,23 @@ bp = Blueprint('search', __name__)
 @bp.route('/search', methods=['GET', 'POST'])
 def search():
     query = request.args.get('query')
+
+    if not query:
+        query = request.args.get('query2')
+
     if not query:
         return jsonify({"error": "No search query provided"}), 400
     
     results = perform_search(query.lower())
+
+    range_min = float('inf')
+    range_max = 0
+
+    for r in results:
+        if r['price'] < range_min:
+            range_min = r['price']
+        if r['price'] > range_max:
+            range_max = r['price']
 
     min_review = request.args.get('min_review', 0, type=float)
     price_min = request.args.get('price_min', 0, type=float)
@@ -23,13 +36,32 @@ def search():
     for r in results:
         if r['category_name'] not in categories:
             categories.append(r['category_name'])
+
     products = results
 
     if min_review or price_min or price_max < float('inf') or category:
         products = []
         for r in results:
-            if r['price'] >= price_min and r['price'] <= price_max and r['category_name'] == category and r['avg_review_score'] >= min_review:
+            if r['price'] >= price_min and r['price'] <= price_max and r['avg_review_score'] >= min_review and (not category or r['category_name'] == category ):
                 products.append(r)
+
+
+    sort_setting = request.args.get('sort_by', 'relevance')
+    asc = request.args.get('sort_order', 'desc') == 'asc'
+
+    if sort_setting == 'relevance':
+        products = sorted(products, key=lambda x: x['relevance'], reverse=not asc)
+
+    elif sort_setting == 'price':
+        products = sorted(products, key=lambda x: x['price'], reverse=not asc)
+
+    elif sort_setting == 'rating':
+        # for ties, sort by review count
+        products = sorted(products, key=lambda x: (x['avg_review_score'], x['review_count']), reverse=not asc)
+
+    else:
+        products = sorted(products, key=lambda x: x['review_count'], reverse=not asc)       
+
 
     return render_template('search.html',
                            products=products,
@@ -38,7 +70,11 @@ def search():
                            price_min=price_min,
                            price_max=price_max,
                            category=category,
-                           categories=categories)
+                           categories=categories,
+                           range_min=range_min,
+                           range_max=range_max,
+                           sort_by=sort_setting,
+                           sort_order='asc' if asc else 'desc')
 
 
 def perform_search(query):
