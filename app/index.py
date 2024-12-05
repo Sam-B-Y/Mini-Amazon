3963dfc1e7df0da0b527a57120815646479305d3
AmranHudda <amhudda@gmail.com>
2024-12-05 17:32:40 -0500

    Seasonal and top-selling

--- app/index.py
+++ app/index.py
@@ -1,25 +1,21 @@
 from flask import render_template
 from flask_login import current_user
-import datetime
 
 from .models.product import Product
-from .models.purchase import Purchase
 
 from flask import Blueprint
 bp = Blueprint('index', __name__)
 
-
 @bp.route('/')
 def index():
-    # get all available products for sale:
-    products = Product.get_all()
-    # find the products current user has bought:
-    if current_user.is_authenticated:
-        purchases = Purchase.get_all_by_user_id_since(
-            current_user.id, datetime.datetime(1980, 9, 14, 0, 0, 0))
-    else:
-        purchases = None
-    # render the page by adding information to the index.html file
+    # Fetch top-selling products (Hot Products)
+    hot_products = Product.get_top_selling_products(limit=10)
+
+    print(hot_products)
+
+    # Fetch Christmas products
+    christmas_products = Product.search_by_keyword('christmas')
+
     return render_template('index.html',
-                           avail_products=products,
-                           purchase_history=purchases)
+                           hot_products=hot_products,
+                           christmas_products=christmas_products)
